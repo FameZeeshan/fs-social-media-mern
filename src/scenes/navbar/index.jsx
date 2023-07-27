40ac2025c4fcac301b9b49116ea0bb55679eae11
FameZeeshan <zeeshan6143@gmail.com>
2023-07-27 11:53:29 +0530

    need to work on theme

--- src/scenes/navbar/index.jsx
+++ src/scenes/navbar/index.jsx
@@ -56,7 +56,7 @@ const Navbar = () => {
             },
           }}
         >
-          Sociopedia
+          Friendify
         </Typography>
         {isNonMobileScreens && (
           <FlexBetween
